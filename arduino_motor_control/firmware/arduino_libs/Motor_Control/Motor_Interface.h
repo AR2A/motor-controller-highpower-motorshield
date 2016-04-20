8e08260659d6fbcb104da61eca9488759141c639
g3rald <s1510567003@students.fh-hagenberg.at>
2016-04-20 11:57:54 +0200

    add motor-interface

---
+++ arduino_motor_control/firmware/arduino_libs/Motor_Control/Motor_Interface.h
@@ -0,0 +1,36 @@
+/**
+ * \author   Gerald D.
+ * \brief    motor interface  
+ * \file     Motor_Interface.h
+ * \license  BSD-3-License
+ */
+
+#ifndef MOTOR_INTERFACE_H_
+#define MOTOR_INTERFACE_H_
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+#include <stdint.h>
+
+#include <Arduino.h>
+
+#include <ros.h>
+#include <Wire.h>
+
+#include <arduino_motor_control/get_current.h>
+#include <arduino_motor_control/set_speed_direction.h>
+
+/**************************************************************************************
+ * INTERFACE
+ **************************************************************************************/
+class MotorInterface {
+public:
+	// empty virtual destructor for proper cleanup
+	virtual ~MotorInterface() {
+	}
+
+	virtual void SetSpeed(const arduino_motor_control::speed & s) = 0;
+};
+
+#endif //MOTOR_INTERFACE_H_
