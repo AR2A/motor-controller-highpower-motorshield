bc8f277b5c9e3180e4217bf9b9fe04b1c2731056
g3rald <s1510567003@students.fh-hagenberg.at>
2016-05-04 15:14:23 +0200

    add comments

--- arduino_motor_control/firmware/arduino_libs/Motor_Control/Motor_Interface.h
+++ arduino_motor_control/firmware/arduino_libs/Motor_Control/Motor_Interface.h
@@ -29,7 +29,14 @@ class MotorInterface {
     // empty virtual destructor for proper cleanup
     virtual ~MotorInterface() {
     }
-
+    
+    /* This function sets the motor speed and the value has to be in the range 
+       from 1.0 to -1.0 :
+        value:  0.0   -> stop
+        value: |1.0|  -> fastest speed
+	positive value-> forward 
+        negative value-> backward
+    */
     virtual void SetSpeed(const arduino_motor_control::speed & s) = 0;
 };
 
